0f79e223ea6c681e4756d7e3dab474d116de2785
flamingo <rok.mokorel@gmail.com>
2020-08-03 20:33:45 +0200

    response from server with Promise-resolve - working in app.js

--- app.js
+++ app.js
@@ -1,77 +1,64 @@
-const a = [
-    {
-      newsHeader: 'Microsoft continue TikTok talks, SpaceX crew splashes down',
-      category: 'videos',
-      url: 'https://www.cnet.com/videos/microsoft-continue-tiktok-talks-spacex-crew-splashes-down/'
-    },
-    {
-      newsHeader: 'AirPods vs. AirPods Pro: Should you spend the extra $100?',
-      category: 'news',
-      url: 'https://www.cnet.com/news/apple-airpods-versus-airpods-pro-cost-100-dollars-more-main-differences-true-wireless-earbuds/'
-    },
-    {
-      newsHeader: '2020 Toyota Tacoma TRD Off-Road review: Rough around the edges',
-      category: 'roadshow',
-      url: 'https://www.cnet.com/roadshow/reviews/2020-toyota-tacoma-review/'
-    },
-    {
-      newsHeader: 'Spotify vs. Apple Music: Which is right for you?',
-      category: 'news',
-      url: 'https://www.cnet.com/news/apple-music-versus-spotify-best-music-podcasts-streaming-service-price-catalog-features-plans-compared/'
-    },
-    {
-      newsHeader: 'How soon might the next stimulus check get to you? Here are some potential IRS payment dates',
-      category: 'personal-finance',
-      url: 'https://www.cnet.com/personal-finance/how-soon-might-the-next-stimulus-payment-get-to-you-here-are-some-potential-irs-payment-dates/'
-    }
-  ];
-  
-  const b = [
-    {
-      shortSummary: 'Ponderous and uncomfortable, this midsize truck is somehow still hugely popular.',
-      tags: 'Toyota Tacoma | Toyota | Trucks | Ford | Toyota | ',
-      author: 'Craig Cole',
-      datePublished: 'Aug. 3, 2020 2:00 a.m. PT',
-      mainImageUrl: 'https://cnet3.cbsistatic.com/img/vRs1tFXEMWGX08nI2vPE2gBUmxw=/2020/07/14/716eb347-8cab-4361-be0f-285bc4db4584/2020-toyota-tacoma-trd-off-road-16.jpg'
-    },
-    {
-      shortSummary: 'Ponderous and uncomfortable, this midsize truck is somehow still hugely popular.',
-      tags: 'Toyota Tacoma | Toyota | Trucks | Ford | Toyota | ',
-      author: 'Craig Cole',
-      datePublished: 'Aug. 3, 2020 2:00 a.m. PT',
-      mainImageUrl: 'https://cnet3.cbsistatic.com/img/vRs1tFXEMWGX08nI2vPE2gBUmxw=/2020/07/14/716eb347-8cab-4361-be0f-285bc4db4584/2020-toyota-tacoma-trd-off-road-16.jpg'
-    },
-    {
-      shortSummary: 'Ponderous and uncomfortable, this midsize truck is somehow still hugely popular.',
-      tags: 'Toyota Tacoma | Toyota | Trucks | Ford | Toyota | ',
-      author: 'Craig Cole',
-      datePublished: 'Aug. 3, 2020 2:00 a.m. PT',
-      mainImageUrl: 'https://cnet3.cbsistatic.com/img/vRs1tFXEMWGX08nI2vPE2gBUmxw=/2020/07/14/716eb347-8cab-4361-be0f-285bc4db4584/2020-toyota-tacoma-trd-off-road-16.jpg'
-    },
-    {
-      shortSummary: 'Ponderous and uncomfortable, this midsize truck is somehow still hugely popular.',
-      tags: 'Toyota Tacoma | Toyota | Trucks | Ford | Toyota | ',
-      author: 'Craig Cole',
-      datePublished: 'Aug. 3, 2020 2:00 a.m. PT',
-      mainImageUrl: 'https://cnet3.cbsistatic.com/img/vRs1tFXEMWGX08nI2vPE2gBUmxw=/2020/07/14/716eb347-8cab-4361-be0f-285bc4db4584/2020-toyota-tacoma-trd-off-road-16.jpg'
-    },
-    {
-      shortSummary: 'Ponderous and uncomfortable, this midsize truck is somehow still hugely popular.',
-      tags: 'Toyota Tacoma | Toyota | Trucks | Ford | Toyota | ',
-      author: 'Craig Cole',
-      datePublished: 'Aug. 3, 2020 2:00 a.m. PT',
-      mainImageUrl: 'https://cnet3.cbsistatic.com/img/vRs1tFXEMWGX08nI2vPE2gBUmxw=/2020/07/14/716eb347-8cab-4361-be0f-285bc4db4584/2020-toyota-tacoma-trd-off-road-16.jpg'
-    }
-  ];
-
-  var d = [];
 
 
-  
-  for (var i = 0; i < 5; i++) {
-      d[i] = {
-        ...a[i],
-        ...b[i],
+
+function scrapeCnet() {
+  result = (async () => {
+    var a =
+    {
+      prvi: {
+        ime: 'Rok',
+        priimek: 'Mokorel',
+        starost: 25
+      },
+      drugi: {
+        ime: 'Aleksandra',
+        priimek: 'Krajnovic',
+        starost: 23
+      }
     }
-  };
-  console.log(d);
+    setTimeout(() => console.log('done'), 1000);
+    return a;
+  })();
+  console.log(result);
+  return JSON.stringify(result);
+}
+
+function someAsyncFunc() {
+  return new Promise(resolve => {
+    var data = (async () => {
+      var a =
+      {
+        prvi: {
+          ime: 'Rok',
+          priimek: 'Mokorel',
+          starost: 25
+        },
+        drugi: {
+          ime: 'Aleksandra',
+          priimek: 'Krajnovic',
+          starost: 23
+        }
+      }
+      return a;
+    })();
+    resolve(data);
+  });
+}
+
+var http = require('http');
+const { stringify } = require('querystring');
+
+const hostname = '127.0.0.1';
+const port = 5060;
+
+const server = http.createServer();
+server.on('request', async (req, res) => {
+  const data = await someAsyncFunc();
+  console.log(req.url);
+  console.log(data);
+  res.end(JSON.stringify(data));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
--- index.js
+++ index.js
@@ -1,89 +1,105 @@
+const http = require('http');
 const puppeteer = require('puppeteer');
+
 const CNET_URL = `https://www.cnet.com/`;
 
-
-(async () => {
+function scrapeForData() {
+  /* immediately invoked function expression */
+  result = (async () => {
     try {
-        /* Initiate the Puppeteer browser */
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        /* Go to the webpage and wait for it to load */
-        await page.goto(CNET_URL, { waitUntil: 'networkidle0' });
-
-        /* Run javascript inside of the page */
-        var mainPageData = await page.evaluate(() => {
-            var storiesNodeList = document.querySelectorAll('div[class="latestScrollItems"] > div[class="row item"] > .col-5 > .col-4 > h3 > a');
-            var storiesArray = [];
-            /* Iterate first 5 latest stories and save data in array */
-            for (var i = 0; i < 5; i++) {
-                storiesArray[i] = {
-                    newsHeader: storiesNodeList[i].innerText,
-                    category: storiesNodeList[i].href.replace("https://www.cnet.com/", "").split('/')[0],
-                    url: storiesNodeList[i].href,
-                };
-            }
-            return storiesArray;
-        });
-        /* Iterate links to 5 latest stories and collect data from subpages */
-        var subPageData = [];
-        // TODO : increase the number of iterations to the number of stories
+      /* Initiate the Puppeteer browser */
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+  
+      /* Go to the webpage and wait for it to load */
+      await page.goto(CNET_URL, { waitUntil: 'networkidle0' });
+  
+      /* Run javascript inside of the page */
+      var mainPageData = await page.evaluate(() => {
+        var storiesNodeList = document.querySelectorAll('div[class="latestScrollItems"] > div[class="row item"] > .col-5 > .col-4 > h3 > a');
+        var storiesArray = [];
+        /* Iterate first 5 latest stories and save data in array */
         for (var i = 0; i < 5; i++) {
-            try {
-                await page.goto(mainPageData[i].url);
-                console.log('Opened page:  ', mainPageData[i].url);
-                subPageData[i] = await page.evaluate(() => {
-                    /* use devtools API to get data */
-                    /* TODO  : Handle video stories, handle */
-                    try {
-                        var shortSummary = document.querySelector('p[class="c-head_dek"]').innerText;
-                        var tags = Array.from(document.querySelectorAll('div[class="row tagList desktop"] > a'), x => x.innerText);
-                        var author = Array.from(document.querySelectorAll('div[class="c-assetAuthor_authors"] > span > .author'), x => x.innerText);
-                        var datePublished = document.querySelector('div[class="c-assetAuthor_date"] > time').innerText;
-                        var mainImageUrl = document.querySelector('article > div > figure[section="shortcodeImage"] > span > span > img').src;
-                    } catch (TypeError) {
-                        var shortSummary = "";
-                        var tags = [""];
-                        var author = [""];
-                        var datePublished = "";
-                        var mainImageUrl = "";
-                    }
-
-                    /* save scraped data from subpages */
-                    subStoriesArray = {
-                        shortSummary: shortSummary,
-                        tags: tags.join(' | '),
-                        author: author.join(' | '),
-                        datePublished: datePublished,
-                        mainImageUrl: mainImageUrl
-                    };
-
-                    return subStoriesArray;
-
-                });
-            } catch (err) {
-                console.log(err);
-            }
+          storiesArray[i] = {
+            newsHeader: storiesNodeList[i].innerText,
+            category: storiesNodeList[i].href.replace("https://www.cnet.com/", "").split('/')[0],
+            url: storiesNodeList[i].href,
+          };
         }
-        /* merge collected data from main and subsites into one array */
-        data = [];
-        for (var i = 0; i < 5; i++) {
-            data[i] = {
-              ...mainPageData[i],
-              ...subPageData[i],
-          }
-        };
-
-        /* Log the results */
-        // console.log(mainPageData);
-        // console.log(subPageData);
-        console.log(data);
-        await browser.close();
-
+        return storiesArray;
+      });
+      /* Iterate links to 5 latest stories and collect data from subpages */
+      var subPageData = [];
+      for (var i = 0; i < 5; i++) {
+        try {
+          await page.goto(mainPageData[i].url);
+          console.log('Opened page:  ', mainPageData[i].url);
+          subPageData[i] = await page.evaluate(() => {
+            /* use devtools API to get data */
+            /* TODO  : Handle video stories, handle */
+            try {
+              var shortSummary = document.querySelector('p[class="c-head_dek"]').innerText;
+              var tags = Array.from(document.querySelectorAll('div[class="row tagList desktop"] > a'), x => x.innerText);
+              var author = Array.from(document.querySelectorAll('div[class="c-assetAuthor_authors"] > span > .author'), x => x.innerText);
+              var datePublished = document.querySelector('div[class="c-assetAuthor_date"] > time').innerText;
+              var mainImageUrl = document.querySelector('article > div > figure[section="shortcodeImage"] > span > span > img').src;
+            } catch (TypeError) {
+              var shortSummary = "";
+              var tags = [""];
+              var author = [""];
+              var datePublished = "";
+              var mainImageUrl = "";
+            }
+  
+            /* combine scraped data from subpages */
+            subStoriesArray = {
+              shortSummary: shortSummary,
+              tags: tags.join(' | '),
+              author: author.join(' | '),
+              datePublished: datePublished,
+              mainImageUrl: mainImageUrl
+            };
+  
+            return subStoriesArray;
+  
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      /* merge collected data from main and subsites into object */
+      data = {};
+      for (var i = 0; i < 5; i++) {
+        data["article " + String(i + 1)] = {
+          ...mainPageData[i],
+          ...subPageData[i],
+        }
+      };
+  
+      /* Log the results */
+      await browser.close();
     }
     catch (err) {
-        console.log(err);
-        await browser.close();
+      console.log(err);
+      await browser.close();
     }
+    return data;
+  })();
 
-})();
+  return JSON.stringify(result);
+}
+
+/* setup of the nodeJS server on localhost */
+/*  */
+const hostname = '127.0.0.1';
+const port = 5060;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'aplication/json');
+  res.end(scrapeForData());
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
